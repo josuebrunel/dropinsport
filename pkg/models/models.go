0a8974d2c2f185d62cb67d7372c80f659008578a
Josue Kouka <josuebrunel@gmail.com>
2024-07-13 17:58:26 -0700

    add form tags to models

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -51,69 +51,69 @@ type Sport struct {
 }
 
 type Group struct {
-	ID             string `json:"id,omitempty"`
-	User           string `json:"user"`
-	City           string `json:"city"`
+	ID             string `json:"id,omitempty" form:"id"`
+	User           string `json:"user" form:"user"`
+	City           string `json:"city" form:"city"`
 	CollectionID   string `json:"collectionId"`
 	CollectionName string `json:"collectionName"`
-	Country        string `json:"country"`
-	Created        string `json:"created"`
-	Description    string `json:"description"`
-	Name           string `json:"name"`
-	Sport          string `json:"sport"`
-	Street         string `json:"street"`
-	Updated        string `json:"updated"`
+	Country        string `json:"country" form:"country"`
+	Created        string `json:"created" form:"created"`
+	Description    string `json:"description" form:"description"`
+	Name           string `json:"name" form:"name"`
+	Sport          string `json:"sport" form:"sport"`
+	Street         string `json:"street" form:"street"`
+	Updated        string `json:"updated" form:"updated"`
 	Expand         struct {
-		Members []Member `json:"members_via_group"`
-		Seasons []Season `json:"seasons_via_group"`
-		Sport   Sport    `json:"sport"`
-	} `json:"expand,omitempty"`
+		Members []Member `json:"members_via_group" form:"members_via_group"`
+		Seasons []Season `json:"seasons_via_group" form:"seasons_via_group"`
+		Sport   Sport    `json:"sport" form:"sport"`
+	} `json:"expand,omitempty" form:"expand"`
 }
 
 type Season struct {
-	ID             string `json:"id,omitempty"`
+	ID             string `json:"id,omitempty" form:"id"`
 	CollectionID   string `json:"collectionId"`
 	CollectionName string `json:"collectionName"`
-	Created        string `json:"created"`
-	Updated        string `json:"updated"`
-	Name           string `json:"name"`
-	Status         string `json:"status"`
-	StartDate      string `json:"start_date"`
-	EndDate        string `json:"end_date"`
-	Group          string `json:"group"`
+	Created        string `json:"created" form:"created"`
+	Updated        string `json:"updated" form:"updated"`
+	Name           string `json:"name" form:"name"`
+	Status         string `json:"status" form:"status"`
+	StartDate      string `json:"start_date" form:"start_date"`
+	EndDate        string `json:"end_date" form:"end_date"`
+	Group          string `json:"group" form:"group"`
 	Expand         struct {
-		Group Group `json:"group"`
-	} `json:"expand,omitempty"`
+		Group Group `json:"group" form:"group"`
+	} `json:"expand,omitempty" form:"expand"`
 }
 
 type Member struct {
-	ID             string `json:"id,omitempty"`
+	ID             string `json:"id,omitempty" form:"id"`
 	CollectionID   string `json:"collectionId"`
 	CollectionName string `json:"collectionName"`
-	Created        string `json:"created"`
-	Updated        string `json:"updated"`
-	Group          string `json:"group"`
-	Username       string `json:"username"`
-	Email          string `json:"email"`
-	Phone          string `json:"phone"`
+	Created        string `json:"created" form:"created"`
+	Updated        string `json:"updated" form:"updated"`
+	Group          string `json:"group" form:"group"`
+	Username       string `json:"username" form:"username"`
+	Email          string `json:"email" form:"email"`
+	Phone          string `json:"phone" form:"phone"`
 	Expand         struct {
-		Group Group `json:"group"`
-	} `json:"expand,omitempty"`
+		Group Group `json:"group" form:"group"`
+	} `json:"expand,omitempty" form:"expand"`
 }
 
 type MemberStat struct {
-	ID             string    `json:"id,omitempty"`
+	ID             string    `json:"id,omitempty" form:"id"`
 	CollectionID   string    `json:"collectionId"`
 	CollectionName string    `json:"collectionName"`
-	Created        string    `json:"created"`
-	Updated        string    `json:"updated"`
-	Group          string    `json:"group"`
-	Member         string    `json:"member"`
-	Season         string    `json:"season"`
-	Stats          SportStat `json:"stats"`
+	Created        string    `json:"created" form:"created"`
+	Updated        string    `json:"updated" form:"updated"`
+	Group          string    `json:"group" form:"group"`
+	Member         string    `json:"member" form:"member"`
+	Season         string    `json:"season" form:"season"`
+	Stats          SportStat `json:"stats" form:"stats"`
 	Expand         struct {
-		Group  Group  `json:"group"`
-		Member Member `json:"member"`
-		Season Season `json:"season"`
-	} `json:"expand,omitempty"`
+		Group  Group  `json:"group" form:"group"`
+		Member Member `json:"member" form:"member"`
+		Season Season `json:"season" form:"season"`
+	} `json:"expand,omitempty" form:"expand"`
 }
