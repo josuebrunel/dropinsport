ecbc0eb7682650fc4e517e377e44f666aff617ab
Josue Kouka <josuebrunel@gmail.com>
2024-07-29 19:57:56 -0700

    improve javascript selection switcher

--- public/js/sportix.js
+++ public/js/sportix.js
@@ -1,13 +1,19 @@
-const selection = document.querySelector('.selection');
+(function () {
+    let selection = document.querySelector('.selection');
 
-selection.addEventListener('click', (event) => {
-    const links = selection.querySelectorAll('a');
-    console.log(links);
-    links.forEach((link) => {
-        if (event.target.id === link.id) {
-            link.classList.remove('outline');
-        } else {
-            link.classList.add('outline');
-        }
-    });
-});
+    if (selection !== null) {
+        selection.addEventListener('click', (event) => {
+            const links = selection.querySelectorAll('a');
+            console.log(links);
+            links.forEach((link) => {
+                if (event.target.id === link.id) {
+                    link.classList.remove('outline');
+                } else {
+                    link.classList.add('outline');
+                }
+            });
+        });
+    }
+})()
+
+
